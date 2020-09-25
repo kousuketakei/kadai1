8f8c4b19b60ba2d9d5b7eb1b813e3d58e01ce7a2
kousuketakei <tom20100320forever@gmail.com>
2020-09-25 19:58:19 +0900

    1回目

--- kadai1/ViewController.swift
+++ kadai1/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    @IBOutlet weak var label1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func button1(_ sender: Any) {
+        
+        //入力
+        let num1 = (textField1.text! as NSString).integerValue
+        let num2 = (textField2.text! as NSString).integerValue
+        let num3 = (textField3.text! as NSString).integerValue
+        let num4 = (textField4.text! as NSString).integerValue
+        let num5 = (textField5.text! as NSString).integerValue
+        
+        //処理
+        
+        let result = num1 + num2 + num3 + num4 + num5
+        
+        //出力
+        label1.text = "\(result)"
+        
+    }
+    
+    
+    
 }
+
+
+
+
 
